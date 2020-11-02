ba9460308db764394e532753bb7812137b529ce9
prodavies <39768603+prodavies@users.noreply.github.com>
2020-11-02 20:37:31 +0300

    updating the database connection

--- config/database.php
+++ config/database.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Str;
+/**This is for heroku postgre database hosting */
+$DATABASE_URL = parse_url('postgres://vrggppengytlsr:77d426a73d92430c22d2400e4961d4ca512946074a09ebea751b8981b8e97559@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d7u0s15p8oaf91');
 
 return [
 
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL[""],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
