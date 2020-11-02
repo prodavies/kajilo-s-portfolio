a8ee1a507c7c0e042bb7f4691a737331002a536a
prodavies <39768603+prodavies@users.noreply.github.com>
2020-11-02 19:59:41 +0300

    better version

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ContactMe;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $messages = ContactMe::get();
+        return view('home', compact('messages'));
     }
     
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,43 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <table id="dataTable" class="table table-striped table-advance table-hover ">
+                        <thead style="background-color: #ff33ff">
+                            <tr>
+                            <th>id</th>
+                            <th>Customer Name</th>
+                            <th>Customer Email</th>
+                            <th>Customer Subject</th>
+                            <th>Customer Message</th>
+                        </tr>
+                    </thead>
+                        <tbody> 
+                            @php
+                                $id = 1;
+                            @endphp
+                 @foreach ($messages as $message)
+                    <tr>
+                        <td>{{ $id }}</td>
+                    <td>{{ $message->name }}</td>
+                    <td>{{ $message->email }}</td>
+                    <td>{{ $message->subject }}</td>
+                    <td>{{ $message->message }}</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr></tr>
+                    </tfoot>
+                    @php
+                    $id++;
+                 @endphp 
+             @endforeach
+            
+            </table>
+                    
                 </div>
             </div>
         </div>
