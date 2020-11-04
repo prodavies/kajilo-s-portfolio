815fe731bb43dd41a6beeacdf0c9329b98cf17f7
prodavies <39768603+prodavies@users.noreply.github.com>
2020-11-04 13:41:08 +0300

    This is changes on css

--- config/database.php
+++ config/database.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-/**This is for heroku postgre database hosting */
-$DATABASE_URL = parse_url('postgres://vrggppengytlsr:77d426a73d92430c22d2400e4961d4ca512946074a09ebea751b8981b8e97559@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d7u0s15p8oaf91');
 
 return [
 
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,11 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"],"/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
